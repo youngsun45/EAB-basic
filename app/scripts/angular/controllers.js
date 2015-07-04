837c14de3438b2dcdc12ccd87245b886ca30381e
youngsun45 <youngsun45@gmail.com>
2015-07-04 13:59:05 +0800

    no changes

--- app/scripts/angular/controllers.js
+++ app/scripts/angular/controllers.js
@@ -2,4 +2,7 @@
 
 app.controller('WelcomeCtrl',function ($scope){
 	$scope.welcome = 'welcome.html的问候!';
+	//$( "a" ).click(function( event ) {
+        //alert( "Thanks for visiting!" );
+   // });
 });
